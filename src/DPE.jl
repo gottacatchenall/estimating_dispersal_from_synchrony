825aea8e6ff501d302bdf07d063279bf4eeec049
michael catchen <mdcatchen@gmail.com>
2020-09-24 11:34:29 -0700

    adding module structure

--- src/DPE.jl
+++ src/DPE.jl
@@ -1,5 +1,33 @@
+# Dispersal Potential Estimator (DPE)
 module DPE
+    
+    # Other libraries
+    using StatsBase
+    using Distributions
+    using DataFrames
+    using CSV
+    using ProgressMeter
+    using LinearAlgebra
+    
+    # Submodules
 
-# Write your package code here.
+    ## Individual-Based Model for generating abundance timeseries
+    ## for testing DPE
+    module IBM
+    end
+
+
+    abstract type DispersalKernel end     
+    abstract type ParameterBundle end
+    abstract type Parameter end
+
+    # Other source files
+
+    include("DispersalPotential.jl")
+    include("Metapopulation.jl")
+    include("Parameters.jl")
+    include("SummaryStats.jl")
+    include("TreatmentFactory.jl")
+
 
 end
